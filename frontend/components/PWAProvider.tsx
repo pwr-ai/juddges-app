49345953d9505f237a6a88e43684cf29ddf9ca5b
Łukasz Augustyniak <aisolutions@lukaszaugustyniak.com>
2026-09-15 22:03:43 +0200

    chore(pwa): remove install app banner
    Refs #632

--- frontend/components/PWAProvider.tsx
+++ frontend/components/PWAProvider.tsx
@@ -1,14 +1,9 @@
 "use client";
 
 import { useServiceWorker } from "@/hooks/useServiceWorker";
-import { PWAInstallPrompt } from "@/components/PWAInstallPrompt";
 
 export function PWAProvider() {
   useServiceWorker();
 
-  return (
-    <>
-      <PWAInstallPrompt />
-    </>
-  );
+  return null;
 }
